5e6f22dd7a4d94060d4f8d6525d5d8ee88163748
Tommy <rocketmanlee1@gmail.com>
2023-02-26 19:25:33 -0500

    Filenaming override added

--- gui.py
+++ gui.py
@@ -21,8 +21,8 @@ class gui(wx.Dialog):
     def __init__(self, parent, id, title):
         # Call the superclass constructor to create the dialog window
         wx.Dialog.__init__(self, parent, id, title,
-                           size=(APP_SIZE_X, APP_SIZE_Y))                   
-
+                           size=(APP_SIZE_X, APP_SIZE_Y))    
+                       
         # Create two vertical box sizers for the two labels and their input controls
         vbox1 = wx.BoxSizer(wx.VERTICAL)
         label1 = wx.StaticText(self, label='Select Roster and Grades:')
@@ -34,31 +34,45 @@ class gui(wx.Dialog):
         label2.SetPosition((30,70))
         vbox2.Add(label2, 0, wx.ALL, 5)
 
+        vbox3 = wx.BoxSizer(wx.VERTICAL)
+        label3 = wx.StaticText(self, label='Certificate Naming Override:')
+        label3.SetPosition((30,190))
+        vbox3.Add(label3, 0, wx.ALL, 5)
+
         # Create a horizontal box sizer to contain the two vertical box sizers
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         hbox.Add(vbox1, 0, wx.EXPAND | wx.ALL, 5)
         hbox.Add(vbox2, 0, wx.EXPAND | wx.ALL, 5)
+        hbox.Add(vbox3, 0, wx.EXPAND | wx.ALL, 5)
 
         # Add buttons and radio box to the dialog
         radio_list = ['Default', 'SBA', 'DOIU','NOAA']
         self.file_picker = wx.FilePickerCtrl(self, 1, message='Select Roster and Grades', pos=(30,40), style=wx.FLP_USE_TEXTCTRL)
         self.directory_picker = wx.DirPickerCtrl(self, 2, message='Output Directory', pos=(30,90), style=wx.DIRP_USE_TEXTCTRL)
+        self.text_ctrl = wx.TextCtrl(self, 5, pos=(30,210))
         wx.Button(self, 3, 'Create Certificates', (350, 300))
         self.rbox = wx.RadioBox(self, 4, 'Certificate Type', choices=radio_list, pos=(30,130))
 
         # Set the initial sizes of the input controls
         self.file_picker.SetInitialSize((400, -1))
         self.directory_picker.SetInitialSize((400, -1))
+        self.text_ctrl.SetInitialSize((400, -1))
 
         # Add the input controls to the vertical box sizers
         vbox1.Add(self.file_picker, 0, wx.ALL, 5)
         vbox2.Add(self.directory_picker, 0, wx.ALL, 5)
+        vbox3.Add(self.text_ctrl, 0, wx.ALL, 5)
+
+        # Set the text color to gray and the initial text to temporary text
+        self.text_ctrl.SetForegroundColour(wx.Colour(128, 128, 128))
+        self.text_ctrl.SetValue('Enter Name Override if applicable')
 
         # Bind events to the buttons and radio box
         self.Bind(wx.EVT_FILEPICKER_CHANGED, self.file_select, id=1)
         self.Bind(wx.EVT_DIRPICKER_CHANGED, self.output_select, id=2)
         self.Bind(wx.EVT_BUTTON, self.run, id=3)
         self.rbox.Bind(wx.EVT_RADIOBOX, self.doc_select, id=4)
+        self.text_ctrl.Bind(wx.EVT_SET_FOCUS, self.on_text_ctrl_focus, id=5)
 
         # Center and show the dialog
         self.Centre()
@@ -97,12 +111,22 @@ class gui(wx.Dialog):
             'NOAA': cert_type.NOAA.value
         }
         # Set the global doc_template variable to the selected template file
-        gui.doc_template = templates.get(input, cert_type.DEFAULT.value)
-        
+        gui.doc_template = templates.get(input, cert_type.DEFAULT.value) 
+
+    # Define the on_text_ctrl_focus() method to clear the temporary text
+    def on_text_ctrl_focus(self, event):
+        if self.text_ctrl.GetValue() == 'Enter Name Override if applicable':
+            self.text_ctrl.SetForegroundColour(wx.BLACK)
+            self.text_ctrl.SetValue('')
+        event.Skip()  # Skip the event to allow the focus to be set on the text box
 
     def run(self, event):
+        # Check if override
+        override = self.text_ctrl.GetValue()
+        if (override != 'Enter Name Override if applicable' and len(override) > 0 and gui.param1 and gui.param2):
+                test.create_docs(gui.roster_and_grades, gui.save_directory, gui.doc_template, override)
         # Check if both parameters have been set
-        if(gui.param1 and gui.param2):
+        elif (gui.param1 and gui.param2 and override == 'Enter Name Override if applicable'):
             test.create_docs(gui.roster_and_grades, gui.save_directory, gui.doc_template)
             # Check for errors
             if(gui.error_1_bool == True):
--- test.py
+++ test.py
@@ -4,21 +4,23 @@ from docxtpl import DocxTemplate
 from docx2pdf import convert
 import os
 
+
 # Instantiates variables for testing
 def main():
     roster_and_grades = 'BMRA Roster and Grades - 11023.0001.xlsx'
     save_directory = 'Certs'
     doc = 'Certificate of Training - SBA Edit.docx'
-    create_docs(roster_and_grades, save_directory, doc)
+    create_docs(roster_and_grades, save_directory, doc,)
 
 # Creates the docs w/correct names and converts them to PDF
-def create_docs(roster_and_grades, save_directory, doc):
+def create_docs(roster_and_grades, save_directory, doc, code_override=None):
+
     doc = DocxTemplate(doc)
     wb = load_workbook(roster_and_grades)
     ws = wb.active
 
     certificate_number = 1
-    for row in ws['2:35']:
+    for row in ws['2:3']:
 
         # Data scraped from the excel
         course_code = str(row[0].value)
@@ -63,7 +65,10 @@ def create_docs(roster_and_grades, save_directory, doc):
 
             # Creates the docs in desired directory
             doc.render(template_fill)
-            document_name = save_directory + "/" + str(certificate_number) + " - " + first_name + " " + last_name + " - Certificate of Training " + course_code + ".docx"
+            if (code_override != None):
+                document_name = save_directory + "/" + str(certificate_number) + " - " + first_name + " " + last_name + " - Certificate of Training - " + code_override + ".docx"
+            else:
+                document_name = save_directory + "/" + str(certificate_number) + " - " + first_name + " " + last_name + " - Certificate of Training - " + course_code + ".docx"
             doc.save(document_name)
             certificate_number += 1
 
